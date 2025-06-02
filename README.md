6572e613945c16f386e43e725042a8b4082e021c
Rahber-1 <rahbardeshmukh@gmail.com>
2025-06-02 16:22:37 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+### How to setup wallet using cast
+
+```shell
+# Account setup
+PK=...
+ACCOUNT=dev
+cast wallet import --private-key $PK $ACCOUNT
+```
+
+### How to execute scripts
+
+```shell
+# Forge scripts
+FORK_URL=...
+ETHERSCAN_API_KEY=...
+MSG_SENDER=...
+
+# Example command to deploy a proxy contract
+forge script script/deploy_proxy.sol:ProxyScript \
+--rpc-url $FORK_URL \
+-vvv \
+--keystore ~/.foundry/keystores/my_keystore.json \
+--sender $MSG_SENDER \
+--broadcast \
+--verify \
+--etherscan-api-key $ETHERSCAN_API_KEY
+```
+
+### FlashLev scripts
+
+Theses are the setups to open and close a leveraged position using the `FlashLev` contract.
+
+#### Important Notice ⚠️
+
+> **Warning:**  
+> Only have one active leveraged position per proxy. Otherwise Aave health factor will return the calculation of all open positions.
+
+0. Setup wallet with `cast`
+1. Edit [configuration](./script/config.sol)
+2. Deploy proxy ([script](./script/deploy_proxy.sol))
+3. Approve token transfers ([script](./script/approve.sol))
+4. Open position ([script](./script/open.sol))
+5. Monitor stats ([script](./script/stats.sol))
+6. Close position ([script](./script/close.sol))
---
+++ src/exercises/SwapRocketPool.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {IRETH} from "../interfaces/rocket-pool/IRETH.sol";
+import {IRocketDepositPool} from
+    "../interfaces/rocket-pool/IRocketDepositPool.sol";
+import {IRocketDAOProtocolSettingsDeposit} from
+    "../interfaces/rocket-pool/IRocketDAOProtocolSettingsDeposit.sol";
+import {IRocketStorage} from "../interfaces/rocket-pool/IRocketStorage.sol";
+import {
+    RETH,
+    ROCKET_STORAGE,
+    ROCKET_DEPOSIT_POOL,
+    ROCKET_DAO_PROTOCOL_SETTINGS_DEPOSIT
+} from "../Constants.sol";
+
+/// @title SwapRocketPool
+/// @notice This contract facilitates swapping between ETH and rETH using RocketPool.
+/// @dev The contract interacts with RocketPool's deposit pool, rETH token, and protocol settings.
+contract SwapRocketPool {
+    IRETH public constant reth = IRETH(RETH);
+    IRocketStorage public constant rStorage = IRocketStorage(ROCKET_STORAGE);
+    IRocketDepositPool public constant depositPool =
+        IRocketDepositPool(ROCKET_DEPOSIT_POOL);
+    IRocketDAOProtocolSettingsDeposit public constant protocolSettings =
+        IRocketDAOProtocolSettingsDeposit(ROCKET_DAO_PROTOCOL_SETTINGS_DEPOSIT);
+
+    uint256 constant CALC_BASE = 1e18;
+
+    /// @notice Calculates the amount of rETH received and fee charged for a given ETH amount.
+    /// @param ethAmount The amount of ETH to be converted to rETH.
+    /// @return rEthAmount The calculated amount of rETH to be received.
+    /// @return fee The deposit fee deducted from the ETH amount.
+    function calcEthToReth(uint256 ethAmount)
+        external
+        view
+        returns (uint256 rEthAmount, uint256 fee)
+    {
+        // Write your code here
+        uint256 ethFee = protocolSettings.getDepositFee();
+        //ETH PER rETH ALWAYS GOING TO BE LESS THAN 1
+        fee = (ethAmount * ethFee) / CALC_BASE;
+        ethAmount -= fee;
+        rEthAmount = reth.getRethValue(ethAmount);
+    }
+
+    /// @notice Calculates the amount of ETH for a given rETH amount.
+    /// @param rEthAmount The amount of rETH to be converted to ETH.
+    /// @return ethAmount The calculated amount of ETH to be received.
+    function calcRethToEth(uint256 rEthAmount)
+        external
+        view
+        returns (uint256 ethAmount)
+    {
+        // Write your code here
+        ethAmount = reth.getEthValue(rEthAmount);
+    }
+
+    /// @notice Retrieves the deposit availability status and maximum deposit amount.
+    /// @return depositEnabled Whether deposits are currently enabled.
+    /// @return maxDepositAmount The maximum allowed deposit amount in ETH.
+    function getAvailability()
+        external
+        view
+        returns (bool depositEnabled, uint256 maxDepositAmount)
+    {
+        // Write your code here
+        depositEnabled = protocolSettings.getDepositEnabled();
+        maxDepositAmount = depositPool.getMaximumDepositAmount();
+        return (depositEnabled, maxDepositAmount);
+    }
+
+    /// @notice Retrieves the deposit delay for rETH deposits.
+    /// @return depositDelay The delay in blocks before deposits are processed.
+    function getDepositDelay() public view returns (uint256 depositDelay) {
+        // Write your code here
+        depositDelay = rStorage.getUint(
+            keccak256(
+                abi.encodePacked(
+                    keccak256("dao.protocol.setting.network"),
+                    "network.reth.deposit.delay"
+                )
+            )
+        );
+    }
+
+    /// @notice Retrieves the block number of the last deposit made by a user.
+    /// @param user The address of the user.
+    /// @return lastDepositBlock The block number of the user's last deposit.
+    function getLastDepositBlock(address user)
+        public
+        view
+        returns (uint256 lastDepositBlock)
+    {
+        // Write your code here
+        bytes32 key = keccak256(abi.encodePacked("user.deposit.block", user));
+        lastDepositBlock = rStorage.getUint(key);
+        return lastDepositBlock;
+    }
+
+    /// @notice Swaps ETH to rETH by depositing ETH into the RocketPool deposit pool.
+    /// @dev The caller must send ETH with this transaction.
+    function swapEthToReth() external payable {
+        // Write your code here
+        depositPool.deposit{value: msg.value}();
+    }
+
+    receive() external payable {}
+
+    /// @notice Swaps rETH to ETH by burning rETH.
+    /// @param rEthAmount The amount of rETH to be burned.
+    /// @dev The caller must approve the contract to transfer the specified rETH amount.
+    function swapRethToEth(uint256 rEthAmount) external {
+        // Write your code here
+        reth.transferFrom(msg.sender, address(this), rEthAmount);
+        // reth.approve(address(this),rEthAmount);
+        reth.burn(rEthAmount);
+    }
+}
